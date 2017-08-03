8ce031299c1df399bd287b99d3f34436338d0164
tqw214 <tqw214@163.com>
2017-08-03 10:14:06 +0800

    commit

---
+++ app/src/main/java/com/viger/gf_bdj/pro/essence/view/navigation/EssenceNavigationBuilder.java
@@ -0,0 +1,21 @@
+package com.viger.gf_bdj.pro.essence.view.navigation;
+
+import android.content.Context;
+
+import com.viger.gf_bdj.pro.base.view.navigation.NavigationBuilderAdapter;
+
+/**
+ * Created by Administrator on 2017/8/2.
+ */
+
+public class EssenceNavigationBuilder extends NavigationBuilderAdapter {
+
+    public EssenceNavigationBuilder(Context context) {
+        super(context);
+    }
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+}
