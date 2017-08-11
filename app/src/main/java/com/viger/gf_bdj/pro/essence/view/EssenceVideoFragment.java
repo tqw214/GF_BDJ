d83926b5a00d2fb370ee6bb4eb4a5202f8752b42
tqw214 <tqw214@163.com>
2017-08-11 17:54:40 +0800

    commit

--- app/src/main/java/com/viger/gf_bdj/pro/essence/view/EssenceVideoFragment.java
+++ app/src/main/java/com/viger/gf_bdj/pro/essence/view/EssenceVideoFragment.java
@@ -1,17 +1,21 @@
 package com.viger.gf_bdj.pro.essence.view;
 
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.andview.refreshview.XRefreshView;
+import com.andview.refreshview.XRefreshViewFooter;
 import com.viger.gf_bdj.R;
 import com.viger.gf_bdj.bean.PostsListBean;
 import com.viger.gf_bdj.mvp.presenter.impl.MvpBasePresenter;
 import com.viger.gf_bdj.pro.base.presenter.BasePresenter;
 import com.viger.gf_bdj.pro.base.view.BaseFragment;
 import com.viger.gf_bdj.pro.essence.presenter.EssenceVideoPresenter;
+import com.viger.gf_bdj.pro.essence.view.adapter.EssenceVideoAdapter;
 import com.viger.gf_bdj.utils.ToastUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,6 +29,8 @@ public class EssenceVideoFragment extends BaseFragment {
     private EssenceVideoPresenter presenter;
     private XRefreshView xRefreshView;
     private RecyclerView recyclerView;
+    private EssenceVideoAdapter essenceVideoAdapter;
+    public List<PostsListBean.PostList> postLists;
 
     public void setType(int mType) {
         this.mType = mType;
@@ -46,11 +52,35 @@ public class EssenceVideoFragment extends BaseFragment {
 
     @Override
     protected void initContentView(View viewContent) {
-        //初始化
+        postLists = new ArrayList<PostsListBean.PostList>();
+
+        //设置下拉刷新控件
         xRefreshView = viewContent.findViewById(R.id.xrefreshview);
+        xRefreshView.setPullRefreshEnable(true);// 设置是否可以下拉刷新
+        xRefreshView.setPullLoadEnable(true);// 设置是否可以上拉加载
+        xRefreshView.setPinnedTime(1000);// 设置刷新完成以后，headerview固定的时间
+        xRefreshView.setAutoLoadMore(true);// 设置支持自动刷新
+        //xRefreshView.setPreLoadCount(2); //设置静默加载时提前加载的item个数
+
         recyclerView = viewContent.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        essenceVideoAdapter = new EssenceVideoAdapter(postLists, getContext());
+        recyclerView.setAdapter(essenceVideoAdapter);
+        essenceVideoAdapter.setCustomLoadMoreView(new XRefreshViewFooter(getContext()));
 
+        xRefreshView.setXRefreshViewListener(new XRefreshView.SimpleXRefreshListener(){
+            @Override
+            public void onRefresh(boolean isPullDown) {
+                loadData(true);
+            }
+
+            @Override
+            public void onLoadMore(boolean isSilence) {
+                loadData(false);
+            }
+        });
     }
 
     @Override
@@ -58,14 +88,25 @@ public class EssenceVideoFragment extends BaseFragment {
         loadData(true);
     }
 
-    private void loadData(boolean isDownRefresh) {
+    private void loadData(final boolean isDownRefresh) {
         presenter.getEssenceList(mType, isDownRefresh, new BasePresenter.OnUiThreadListener<List<PostsListBean.PostList>>() {
             @Override
             public void onResult(List<PostsListBean.PostList> result) {
+                if(isDownRefresh) {
+                    xRefreshView.startRefresh();
+                }else {
+                    xRefreshView.stopLoadMore();
+                }
                 if(result == null) {
                     ToastUtil.showToast(getContext(), "类型："+mType+" 加载失败");
                 }else {
                     ToastUtil.showToast(getContext(), "类型："+mType+" 加载成功");
+                    //刷新ui
+                    if(isDownRefresh) {
+                        postLists.clear();
+                    }
+                    postLists.addAll(result);
+                    essenceVideoAdapter.notifyDataSetChanged();
                 }
             }
         });
--- app/src/main/java/com/viger/gf_bdj/pro/essence/view/adapter/EssenceVideoAdapter.java
+++ app/src/main/java/com/viger/gf_bdj/pro/essence/view/adapter/EssenceVideoAdapter.java
@@ -1,16 +1,25 @@
 package com.viger.gf_bdj.pro.essence.view.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
 import com.andview.refreshview.recyclerview.BaseRecyclerAdapter;
 import com.viger.gf_bdj.R;
 import com.viger.gf_bdj.bean.PostsListBean;
 import com.viger.gf_bdj.pro.essence.view.views.CircleNetworkImageImage;
+import com.viger.gf_bdj.utils.DateUtils;
+
 import java.util.List;
 
 /**
@@ -22,6 +31,11 @@ public class EssenceVideoAdapter extends BaseRecyclerAdapter<EssenceVideoAdapter
     private Context context;
     private List<PostsListBean.PostList> list;
 
+    public EssenceVideoAdapter(List<PostsListBean.PostList> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
     @Override
     public VideoAdapterViewHolder getViewHolder(View view) {
         return new VideoAdapterViewHolder(view, false);
@@ -29,17 +43,44 @@ public class EssenceVideoAdapter extends BaseRecyclerAdapter<EssenceVideoAdapter
 
     @Override
     public VideoAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType, boolean isItem) {
-        return null;
+        View v = LayoutInflater.from(context).inflate(R.layout.item_essence_video_layout, parent, false);
+        VideoAdapterViewHolder holder = new VideoAdapterViewHolder(v, isItem);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(VideoAdapterViewHolder holder, int position, boolean isItem) {
+        PostsListBean.PostList postList = this.list.get(position);
+        loadImage(holder.iv_header,postList.getProfile_image());
+        holder.tv_name.setText(postList.getName());
+        holder.tv_time.setText(DateUtils.parseDate(postList.getCreate_time()));
+        holder.tv_content.setText(postList.getText());
+        holder.tv_like.setText(postList.getDing());
+        holder.tv_dislike.setText(postList.getCai());
+        holder.tv_forword.setText(postList.getRepost());
+        holder.tv_comment.setText(postList.getComment());
+    }
 
+    //Volley框架
+    private void loadImage(NetworkImageView imageView, String url){
+        RequestQueue queue = Volley.newRequestQueue(context);
+        ImageLoader imageLoader = new ImageLoader(queue, new ImageLoader.ImageCache() {
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+
+            }
+
+            @Override
+            public Bitmap getBitmap(String url) {
+                return null;
+            }
+        });
+        imageView.setImageUrl(url,imageLoader);
     }
 
     @Override
     public int getAdapterItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class VideoAdapterViewHolder extends RecyclerView.ViewHolder {
