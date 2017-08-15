9b29ee683ab67491885344b7c00b778bc9d249fa
tqw214 <tqw214@163.com>
2017-08-15 15:57:23 +0800

    commit

---
+++ app/src/main/java/com/viger/gf_bdj/pro/base/view/item/ItemBuilder.java
@@ -0,0 +1,14 @@
+package com.viger.gf_bdj.pro.base.view.item;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/8/15.
+ */
+
+public interface ItemBuilder {
+
+    public View buildAndBind(ViewGroup parent);
+
+}
