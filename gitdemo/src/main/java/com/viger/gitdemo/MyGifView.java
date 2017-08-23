150bac03e9e2b75f8ec9434895cc86f4bb4d6b7f
tqw214 <tqw214@163.com>
2017-08-23 15:24:37 +0800

    commit

--- gitdemo/src/main/java/com/viger/gitdemo/MyGifView.java
+++ gitdemo/src/main/java/com/viger/gitdemo/MyGifView.java
@@ -2,10 +2,14 @@ package com.viger.gitdemo;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Movie;
 import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Administrator on 2017/8/23.
@@ -14,15 +18,23 @@ import android.view.SurfaceView;
 public class MyGifView extends SurfaceView implements SurfaceHolder.Callback{
 
     private SurfaceHolder mHolder;
+    private Movie mMovie;
 
-    private Handler handler = new Handler();
-    private Runnable runnable = new Runnable() {
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable = new Runnable() {
         @Override
         public void run() {
             Canvas canvas = mHolder.lockCanvas();
             canvas.save();
-
+            //绘制图片
+            mMovie.draw(canvas,0,0);
+            //切换每一帧
+            mMovie.setTime((int) (System.currentTimeMillis() % mMovie.duration()));
+            canvas.restore();
+            //解锁画布
             mHolder.unlockCanvasAndPost(canvas);
+            mHandler.postDelayed(mRunnable, 50);
+            Log.d("tag","run==>"+(int) (System.currentTimeMillis() % mMovie.duration()));
         }
     };
 
@@ -41,9 +53,20 @@ public class MyGifView extends SurfaceView implements SurfaceHolder.Callback{
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
         //创建
-        handler.post(runnable);
+        try {
+            InputStream inputStream = getResources().getAssets().open("wo.gif");
+            mMovie = Movie.decodeStream(inputStream);
+            mHandler.post(mRunnable);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
